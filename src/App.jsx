7eb4a432d8d6f82f9f05944708e946beee46c16c
abdurrahman720 <personal.abdurrahman@gmail.com>
2023-03-08 09:43:38 +0600

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import Main from "./Page/Main";
+
+
 function App() {
   return (
-    <div>
-      <header className="p-4 flex justify-between">
-        <a href="" className="logo flex items-center gap-1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6 -rotate-90"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-            />
-          </svg>
-          <span className="font-bold">AirBnB</span>
-        </a>
-        <div className="flex border border-gray-300 rounded-full py-2 px-4">
-          <div>Anywhere</div>
-          <div className="border-l border-gray-300"></div>
-          <div>Any week</div>
-          <div className="border-l border-gray-300"></div>
-          <div>Add guests</div>
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.75 15.75l-2.489-2.489m0 0a3.375 3.375 0 10-4.773-4.773 3.375 3.375 0 004.774 4.774zM21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-          </button>
-        </div>
-      </header>
-    </div>
+    <Routes>
+      <Route index element={<Main/>}></Route>
+   </Routes>
   );
 }
 
