631c0c39f4aaa9c772d7ae763e25b71cd9916288
abdurrahman720 <personal.abdurrahman@gmail.com>
2023-06-13 16:37:23 +0600

    working on account page

--- src/Page/AccountPage.jsx
+++ src/Page/AccountPage.jsx
@@ -1,9 +1,26 @@
 import React, { useContext } from "react";
 import { UserContext } from "./UserContext";
-import { Link, Navigate } from "react-router-dom";
+import { Link, Navigate, useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 const AccountPage = () => {
-  const { user, loading } = useContext(UserContext);
+    const { user, loading } = useContext(UserContext);
+    const navigate = useNavigate()
+    let { subpage } = useParams();
+    {
+        if (subpage === undefined){
+            subpage = 'profile';
+        }
+    }
+
+    const logOut = async() => {
+        const res = await axios.post('/logout');
+        const data = await res.data;
+     
+            navigate('/')
+    
+    }
+
 
   if (loading) {
     return "loading...";
@@ -11,15 +28,31 @@ const AccountPage = () => {
   if (!user && !loading) {
     return <Navigate to={"/login"} />;
   }
+    
+    const linkClass = (type = null)=> {
+        let classes = 'py-2 px-4';
+        if (type === subpage) {
+            classes += ' bg-primary text-white rounded-full'
+        }
+        return classes;
+}
 
   return (
     <div>
-      <nav className="w-full flex mt-8 gap-2 justify-center ">
-        <Link className="py-2 px-4 bg-primary text-white rounded-full" to={"/account"}>My Account</Link>
-        <Link className="py-2 px-4" to={"/account/bookings"}>My Bookings</Link>
-        <Link className="py-2 px-4" to={"/account/places"}>My Accomodations</Link>
-      </nav>
-    </div>
+    <nav className="w-full flex mt-8 gap-2 justify-center ">
+      <Link className={linkClass('profile')} to={"/account"}>My Account</Link>
+      <Link className={linkClass('bookings')} to={"/account/bookings"}>My Bookings</Link>
+      <Link className={linkClass('places')} to={"/account/places"}>My Accomodations</Link>
+          </nav>
+          {subpage === 'profile' && (
+              <div className='text-center'>
+                  Logged in as {user?.name} and ({user?.email} ) 
+                  <br />
+                  <button onClick={logOut}  className='primary max-w-sm mt-2'>Log Out</button>
+              </div>
+          )}
+  </div>
+  
   );
 };
 
